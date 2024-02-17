67702ac32d00c75939d964adffd2a178ae74c49c
Abhi K <abhigokul.k@gmail.com>
2024-02-17 15:15:43 -0500

    Set up radio messaging system, need to test

--- Reciever/src/main.cpp
+++ Reciever/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
-
+#include <Wire.h>
 // code for Reciever to print hello world from Michael
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
+#define GND_PIN 1
+#define Vcc_PIN 2
+#define CE_PIN 3
+#define CSN_PIN 4
+#define SCLK_PIN 5
+#define MOSI_PIN 6 // used to send?
+#define MISO_PIN 7 // used to receive?
 
 /*
 Reciever To Do:
@@ -11,36 +19,56 @@ Need to recieve:
   - Gyroscope Data
   - Flex Sensor Data
 
-Needed Pin Connections:
+Needed Pin Connections: 
+(Based on chip datasheet from https://components101.com/wireless/nrf24l01-pinout-features-datasheet)
   - GND (Pin 1)
   - Vcc (Pin 2)
-  - 
+  - Chip Enable (Pin 3)
+  - Chip Select Not (Pin 4) needs to be kept high
+  - Serial Clock (Pin 5)
+  - MOSI (Pin 6)
+  - MISO (Pin 7)
+
+IRQ (Pin 8) is not really needed, we are polling for data
 
 Key operations:
-with the gyroscope, we need to:
-  - change steering? based on rotation
-  - after a certain threshold rotation was made, we consider it a steering input
+Recieve Steering and Acceleration values
 
-with the flex sensor, we need to:
-  - change speed based on the "strength" of the input?
-
-
+convert them to needed analog signals to our motors
 
 */
-
-RF24 radio(7, 8); // CE, CSN
+RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
 
 const byte address[6] = "00001";
 
+/*
+Message Structure:
+only sending an integer
+0 = Stationary
+1 = Forward
+2 = Left
+3 = Right
+4 = Error in msg
+*/
+void parseMessage(int message){
+
+}
+
 void setup() {
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
+	Serial.begin(19200);
+  
+	// Set up radio into RX mode. may need to set up payload size
+	radio.begin();
+	radio.openReadingPipe(1, address);
+	radio.setPALevel(RF24_PA_MIN);
+	radio.startListening();
 }
 
 void loop() {
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  delay(1000);
+	if (radio.available()) {
+        int text;
+        radio.read(&text, sizeof(text));
+        Serial.println(text);
+    }
+    //Serial.print("TEST");
 }
--- Transmitter/src/main.cpp
+++ Transmitter/src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <Wire.h>
 // code for Transmitter to print hello world from Michael
 #define led 5
 #include <SPI.h>
@@ -7,29 +7,94 @@
 #include <RF24.h>
 
 /*
-Transmitter To Do:
-Need to send:
-  - Gyroscope Data
-  - Flex Sensor Data
-  - 
+Needed Pin Connections: 
+(Based on chip datasheet from https://components101.com/wireless/nrf24l01-pinout-features-datasheet)
+  - GND (Pin 1)
+  - Vcc (Pin 2)
+  - Chip Enable (Pin 3)
+  - Chip Select Not (Pin 4) needs to be kept high
+  - Serial Clock (Pin 5)
+  - MOSI (Pin 6)
+  - MISO (Pin 7)
+
+IRQ (Pin 8) is not really needed, we are polling for data
 */
 
-RF24 radio(7, 8); // CE, CSN
+#define GND_PIN 1
+#define Vcc_PIN 2
+#define CE_PIN 3
+#define CSN_PIN 4
+#define SCLK_PIN 5
+#define MOSI_PIN 6 // used to send?
+#define MISO_PIN 7 // used to receive?
+
+/*
+Transmitter To Do:
+Need to send:
+    - Steering Data
+    - Acceleration Data
+
+Steering Data:
+    Use Gyroscope
+    - set threshold for roll angle to determine whether we steer or not
+    - send steering direction (not worrying about gradual angle steering, only one steering angle!)
+
+Acceleration Data:
+    Use Flex Sensor
+    - more bending, higher resistance
+    - ranges between 700 to 900, should be mapped to the 0 - 255 PWM range
+    - we will need to convert the incoming 700-900 range into an intended speed value
+    - convert this intended speed value into the PWM range for our motors
+*/
+
+RF24 radio(CE_PIN, CSN_PIN); // CE, CSN
+
+/*
+Message Structure:
+only sending an integer
+0 = Stationary
+1 = Forward
+2 = Left
+3 = Right
+4 = Error in msg
+*/
 
 const byte address[6] = "00001";
 void setup() {
-  Serial.begin(9600);
-  radio.begin();
-  radio.openReadingPipe(0, address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.startListening();
+    Serial.begin(9600);
+    /*
+    Wire.begin();                      // Initialize comunication
+    Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
+    Wire.write(0x6B);                  // Talk to the register 6B
+    Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
+    Wire.endTransmission(true);        //end the transmission
+    */
+    
+    // setup radio into TX mode. may need to implement payload size
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
+
 }
 
+int count = 0;
+bool tx_ok, tx_fail, rx_ready;
+
 void loop() {
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
-  }
-  //Serial.print("TEST");
+    int msg = count;
+	bool report = radio.write(&msg, sizeof(msg));
+    radio.whatHappened(tx_ok, tx_fail, rx_ready);
+	
+    if(report){
+        Serial.print(msg, DEC);
+        Serial.print(" Sent! TX_OK, TX_FAIL, RX_READY");
+		Serial.print(tx_ok, BIN);
+		Serial.print(tx_fail, BIN);
+		Serial.print(rx_ready, BIN);
+		Serial.println();
+    }
+    count++;
+    delay(1000);
+    
 }
